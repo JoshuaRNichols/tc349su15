fc7293dc2504c9685daf8fc6384bf6feec298456
JoshuaRNichols <joshua.r.nichols@gmail.com>
2015-06-10 00:04:02 -0400

    oops

--- rot13.py
+++
@@ -1,5 +0,0 @@
-import string
-rot13 = str.maketrans( 
-    "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", 
-    "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
-print(str.translate("nicho448@msu.edu", rot13))
